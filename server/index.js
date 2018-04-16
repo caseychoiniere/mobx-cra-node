d09229cd4adb39dc1fdf896fdabf7050b67562e9
casey choiniere <choiniere@mbp.local>
2018-04-16 10:43:22 -0700

    Static routing wildcard

--- server/index.js
+++ server/index.js
@@ -25,22 +25,8 @@ app.get('/api', function (req, res) {
 });
 
 // All remaining requests return the React app, so it can handle routing.
-app.get('*', (request, response) => {
+app.get('/*', (request, response) => {
   response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-// const express = require('express');
-// const app = express();
-// const PORT = process.env.PORT || 5000;
-//
-// app.use(express.static(__dirname + '/../react-ui/build'));
-//
-// app.get('/api', function (req, res) {
-//     res.set('Content-Type', 'application/json');
-//     res.send('{"message":"Hello from the custom server!"}');
-// });
-//
-// app.listen(PORT, function () {
-//     console.log(`Listening on port ${PORT}`);
-// });
