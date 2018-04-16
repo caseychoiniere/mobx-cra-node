ff1a53df78d7df7f7853df7461d1d68cb93180cd
casey choiniere <choiniere@mbp.local>
2018-04-16 11:07:51 -0700

    Static routing wildcard

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const cors = require('cors');
 const express = require('express');
-// const path = require('path');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 5000;
 //     app.use(express.static(__dirname + '/../react-ui/build'));
 //     // express.static(path.resolve(__dirname, '../react-ui/build'))
 // });
-app.use(express.static(__dirname + '/../react-ui/build'), cors());
+// app.use(express.static(__dirname + '/../react-ui/build'), cors());
+app.use(express.static(path.resolve(__dirname, '../react-ui/build')), cors());
 // Answer API requests.
 app.get('/data', (req, res) => {
     console.log('api request received')
@@ -25,8 +26,8 @@ app.get('/api', function (req, res) {
 });
 
 // All remaining requests return the React app, so it can handle routing.
-app.get('/*', (request, response) => {
-  response.sendFile(path.resolve(__dirname, '/../react-ui/build', 'index.html'));
+app.get('*', (request, response) => {
+    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
