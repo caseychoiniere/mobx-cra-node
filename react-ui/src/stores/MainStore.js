da58701025a61bc7cb88f3a9dd96a2318ccc606b
casey choiniere <choiniere@mbp.local>
2018-05-22 15:04:45 -0700

    try travis ci

--- react-ui/src/stores/MainStore.js
+++ react-ui/src/stores/MainStore.js
@@ -16,36 +16,40 @@ export class MainStore {
         this.openNav = false;
     }
 
-    @action setAnchorElement(anchorEl, i) {
-        let a = this.anchorElements;
-        !a.has(i) ? a.set(i, anchorEl) : a.delete(i);
-        this.anchorElements = a;
-    }
+    // @action setAnchorElement(anchorEl, i) {
+    //     let a = this.anchorElements;
+    //     !a.has(i) ? a.set(i, anchorEl) : a.delete(i);
+    //     this.anchorElements = a;
+    // }
+    //
+    // @action test() {
+    //     api.test()
+    //         .then(checkStatus)
+    //         .then(response => response.json())
+    //         .then((json) => {
+    //             console.log(json)
+    //         }).catch(er => this.handleErrors(er))
+    // }
 
-    @action test() {
-        api.test()
-            .then(checkStatus)
-            .then(response => response.json())
-            .then((json) => {
-                console.log(json)
-            }).catch(er => this.handleErrors(er))
-    }
+    // @action toggleLoading() {
+    //     this.loading = !this.loading;
+    // }
 
-    @action toggleLoading() {
+    toggleLoading() {
         this.loading = !this.loading;
     }
 
-    @action toggleDrawer(key) {
-        !this.drawers.has(key) ? this.drawers.set(key, true) : this.drawers.delete(key);
-    }
-
-    @action handleErrors(er) {
-        this.loading = false;
-        if (er.response.status === 401) {
-            localStorage.setItem('redirectUrl', window.location.href);
-            AuthStore.logout(er);
-        }
-    }
+    // @action toggleDrawer(key) {
+    //     !this.drawers.has(key) ? this.drawers.set(key, true) : this.drawers.delete(key);
+    // }
+    //
+    // @action handleErrors(er) {
+    //     this.loading = false;
+    //     if (er.response.status === 401) {
+    //         localStorage.setItem('redirectUrl', window.location.href);
+    //         AuthStore.logout(er);
+    //     }
+    // }
 }
 
 const mainStore = new MainStore();
