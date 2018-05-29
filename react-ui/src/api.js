16f3e31245d2133a7d2e99619ba70dffab5872bb
casey choiniere <choiniere@mbp.local>
2018-05-28 17:59:10 -0700

    wtf

--- react-ui/src/api.js
+++ react-ui/src/api.js
@@ -16,7 +16,7 @@ const api = {
     },
 
     getProjects: (token) => {
-        return fetch('https://apidev.dataservice.duke.edu/api/v1/projects?per_page=1000',getFetchParams('get', token))
+        return fetch(`${process.env.REACT_APP_DDS_API_URL}?per_page=1000`,getFetchParams('get', token))
     },
 
     makePlanet: () => {
